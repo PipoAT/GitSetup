4a396409207e7d9e9dc4a0655157e61ff4f47ac8
Andrew Pipo <atpipo2026@gmail.com>
2023-06-26 21:00:05 -0400

    Cleaned up code/addressed warnings and messages

--- GitSetup/Form1.cs
+++ GitSetup/Form1.cs
@@ -1,7 +1,4 @@
-using System;
-using System.ComponentModel.Design;
 using System.Diagnostics;
-using System.Windows.Forms;
 
 namespace GitSetup
 {
@@ -15,122 +12,122 @@ namespace GitSetup
             this.FormBorderStyle = FormBorderStyle.FixedSingle;
             this.Text = "GitSetup";
 
-            Button btnStartSetup = new Button();
-            btnStartSetup.Text = "Start Setup Process";
-            btnStartSetup.Location = new Point(0, 25);
-            btnStartSetup.Width = 250;
-            btnStartSetup.Click += btnStartSetupClick;
+            Button btnStartSetup = new()
+            {
+                Text = "Start Setup Process",
+                Location = new Point(0, 25),
+                Width = 250
+            };
+            btnStartSetup.Click += BtnStartSetupClick!;
             this.Controls.Add(btnStartSetup);
         }
 
-        private void btnStartSetupClick(object sender, EventArgs e)
+        private void BtnStartSetupClick(object sender, EventArgs e)
         {
             GitSetup();
         }
 
-        private void GitSetup()
+        private static void GitSetup()
         {
-            using (var folderBrowserDialog = new FolderBrowserDialog())
+            using var folderBrowserDialog = new FolderBrowserDialog();
+            var url = PromptForURL();
+            if (url != null)
             {
-                var url = PromptForURL();
-                if (url != null)
+                if (folderBrowserDialog.ShowDialog() == DialogResult.OK)
                 {
-                    if (folderBrowserDialog.ShowDialog() == DialogResult.OK)
+                    var workingDirectory = folderBrowserDialog.SelectedPath;
+
+                    var processGitInit = new ProcessStartInfo // git init
                     {
-                        var workingDirectory = folderBrowserDialog.SelectedPath;
+                        FileName = "git",
+                        Arguments = "init",
+                        WorkingDirectory = workingDirectory,
+                        RedirectStandardOutput = true,
+                        RedirectStandardError = true,
+                        UseShellExecute = false,
+                        CreateNoWindow = true
+                    };
 
-                        var processGitInit = new ProcessStartInfo // git init
-                        {
-                            FileName = "git",
-                            Arguments = "init",
-                            WorkingDirectory = workingDirectory,
-                            RedirectStandardOutput = true,
-                            RedirectStandardError = true,
-                            UseShellExecute = false,
-                            CreateNoWindow = true
-                        };
+                    StartProcess(processGitInit);
 
-                        startProcess(processGitInit);
+                    var processGitAddAll = new ProcessStartInfo     // git add -A
+                    {
+                        FileName = "git",
+                        Arguments = "add -A",
+                        WorkingDirectory = workingDirectory,
+                        RedirectStandardOutput = true,
+                        RedirectStandardError = true,
+                        UseShellExecute = false,
+                        CreateNoWindow = true
+                    };
 
-                        var processGitAddAll = new ProcessStartInfo     // git add -A
-                        {
-                            FileName = "git",
-                            Arguments = "add -A",
-                            WorkingDirectory = workingDirectory,
-                            RedirectStandardOutput = true,
-                            RedirectStandardError = true,
-                            UseShellExecute = false,
-                            CreateNoWindow = true
-                        };
+                    StartProcess(processGitAddAll);
 
-                        startProcess(processGitAddAll);
+                    var processGitCommit = new ProcessStartInfo     // git commit
+                    {
+                        FileName = "git",
+                        Arguments = "commit -m \"" + "Project init" + "\"",
+                        WorkingDirectory = workingDirectory,
+                        RedirectStandardOutput = true,
+                        RedirectStandardError = true,
+                        UseShellExecute = false,
+                        CreateNoWindow = true
+                    };
 
-                        var processGitCommit = new ProcessStartInfo     // git commit
-                        {
-                            FileName = "git",
-                            Arguments = "commit -m \"" + "Project init" + "\"",
-                            WorkingDirectory = workingDirectory,
-                            RedirectStandardOutput = true,
-                            RedirectStandardError = true,
-                            UseShellExecute = false,
-                            CreateNoWindow = true
-                        };
+                    StartProcess(processGitCommit);
 
-                        startProcess(processGitCommit);
+                    var processGitBranch = new ProcessStartInfo     // git branch to main
+                    {
+                        FileName = "git",
+                        Arguments = "branch -M main",
+                        WorkingDirectory = workingDirectory,
+                        RedirectStandardOutput = true,
+                        RedirectStandardError = true,
+                        UseShellExecute = false,
+                        CreateNoWindow = true
+                    };
 
-                        var processGitBranch = new ProcessStartInfo     // git branch to main
-                        {
-                            FileName = "git",
-                            Arguments = "branch -M main",
-                            WorkingDirectory = workingDirectory,
-                            RedirectStandardOutput = true,
-                            RedirectStandardError = true,
-                            UseShellExecute = false,
-                            CreateNoWindow = true
-                        };
+                    StartProcess(processGitBranch);
 
-                        startProcess(processGitBranch);
+                    var processGitRemote = new ProcessStartInfo // git remote add
+                    {
+                        FileName = "git",
+                        Arguments = $"remote add origin {url}",
+                        WorkingDirectory = workingDirectory,
+                        RedirectStandardOutput = true,
+                        RedirectStandardError = true,
+                        UseShellExecute = false,
+                        CreateNoWindow = true
+                    };
 
-                        var processGitRemote = new ProcessStartInfo // git remote add
-                        {
-                            FileName = "git",
-                            Arguments = $"remote add origin {url}",
-                            WorkingDirectory = workingDirectory,
-                            RedirectStandardOutput = true,
-                            RedirectStandardError = true,
-                            UseShellExecute = false,
-                            CreateNoWindow = true
-                        };
+                    StartProcess(processGitRemote);
 
-                        startProcess(processGitRemote);
+                    var processGitPush = new ProcessStartInfo     // git push
+                    {
+                        FileName = "git",
+                        Arguments = "push -u origin main",
+                        WorkingDirectory = workingDirectory,
+                        RedirectStandardOutput = true,
+                        RedirectStandardError = true,
+                        UseShellExecute = false,
+                        CreateNoWindow = true
+                    };
 
-                        var processGitPush = new ProcessStartInfo     // git push
-                        {
-                            FileName = "git",
-                            Arguments = "push -u origin main",
-                            WorkingDirectory = workingDirectory,
-                            RedirectStandardOutput = true,
-                            RedirectStandardError = true,
-                            UseShellExecute = false,
-                            CreateNoWindow = true
-                        };
+                    StartProcess(processGitPush);
 
-                        startProcess(processGitPush);
-
-                    }
                 }
             }
         }
-        
+
 
         public class PromptDialog : Form
         {
-            private Label label;
-            private TextBox textBox;
-            private Button okButton;
-            private Button cancelButton;
+            private readonly Label label;
+            private readonly TextBox textBox;
+            private readonly Button okButton;
+            private readonly Button cancelButton;
 
-            public string URL { get; private set; }
+            public string? URL { get; private set; }
 
             public PromptDialog(string title, string labelText)
             {
@@ -141,25 +138,33 @@ namespace GitSetup
                 this.MaximizeBox = false;
                 this.MinimizeBox = false;
 
-                label = new Label();
-                label.AutoSize = true;
-                label.Location = new System.Drawing.Point(15, 20);
-                label.Text = labelText;
+                label = new Label
+                {
+                    AutoSize = true,
+                    Location = new System.Drawing.Point(15, 20),
+                    Text = labelText
+                };
 
-                textBox = new TextBox();
-                textBox.Location = new System.Drawing.Point(15, 40);
-                textBox.Size = new System.Drawing.Size(270, 20);
+                textBox = new TextBox
+                {
+                    Location = new System.Drawing.Point(15, 40),
+                    Size = new System.Drawing.Size(270, 20)
+                };
 
-                okButton = new Button();
-                okButton.DialogResult = DialogResult.OK;
-                okButton.Location = new System.Drawing.Point(130, 70);
-                okButton.Text = "OK";
-                okButton.Click += OkButton_Click;
+                okButton = new Button
+                {
+                    DialogResult = DialogResult.OK,
+                    Location = new System.Drawing.Point(130, 70),
+                    Text = "OK"
+                };
+                okButton.Click += OkButton_Click!;
 
-                cancelButton = new Button();
-                cancelButton.DialogResult = DialogResult.Cancel;
-                cancelButton.Location = new System.Drawing.Point(210, 70);
-                cancelButton.Text = "Cancel";
+                cancelButton = new Button
+                {
+                    DialogResult = DialogResult.Cancel,
+                    Location = new System.Drawing.Point(210, 70),
+                    Text = "Cancel"
+                };
 
                 this.Controls.Add(label);
                 this.Controls.Add(textBox);
@@ -173,7 +178,7 @@ namespace GitSetup
             }
         }
 
-        private string PromptForURL()
+        private static string? PromptForURL()
         {
             var prompt = new PromptDialog("Enter Git Remote URL", "Please enter the Git remote URL:");
             if (prompt.ShowDialog() == DialogResult.OK)
@@ -183,18 +188,16 @@ namespace GitSetup
             return null;
         }
 
-        private void startProcess(ProcessStartInfo processInfo)
+        private static void StartProcess(ProcessStartInfo processInfo)
         {
-            using (var process = new Process())
-            {
-                process.StartInfo = processInfo;
-                process.Start();
+            using var process = new Process();
+            process.StartInfo = processInfo;
+            process.Start();
 
-                var output = process.StandardOutput.ReadToEnd();
-                MessageBox.Show(output);
-                var error = process.StandardError.ReadToEnd();
-                MessageBox.Show(error);
-            }
+            var output = process.StandardOutput.ReadToEnd();
+            MessageBox.Show(output);
+            var error = process.StandardError.ReadToEnd();
+            MessageBox.Show(error);
         }
     }
 }
